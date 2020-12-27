4cf6249b915e76935b1275f91bc0e4cb16e77ce8
n.skorokhod <mtwain.mark@gmail.com>
2020-12-27 23:53:01 +0300

    Создан Maven проект. Добавлены директории и создан тестовый класс. В примере тестирую обращение от файла applicationContext.xml к классу TestBean, передавая в конструктор value

---
+++ src/main/java/ru/skorokhod/springcourse/TestBean.java
@@ -0,0 +1,17 @@
+package ru.skorokhod.springcourse;
+
+public class TestBean {
+    public String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
