69a9f2361e9d342d5fcd3629a40454115ccd892d
n.skorokhod <mtwain.mark@gmail.com>
2020-12-29 17:13:48 +0300

    @Qualifier

--- src/main/java/ru/skorokhod/springcourse/MusicPlayer.java
+++ src/main/java/ru/skorokhod/springcourse/MusicPlayer.java
@@ -1,21 +1,22 @@
 package ru.skorokhod.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic(){
-            return classicalMusic.getSong() + " " + rockMusic.getSong();
+            return music1.getSong() + "\n" + music2.getSong();
     }
 }
