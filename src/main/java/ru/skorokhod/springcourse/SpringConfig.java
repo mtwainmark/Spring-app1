f393b17ebffe4336f7720b0364402511f37a965e
n.skorokhod <mtwain.mark@gmail.com>
2020-12-29 17:46:35 +0300

    Конфигурация с помощью Java кода

---
+++ src/main/java/ru/skorokhod/springcourse/SpringConfig.java
@@ -0,0 +1,10 @@
+package ru.skorokhod.springcourse;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.skorokhod.springcourse")
+public class SpringConfig {
+
+}
--- src/main/java/ru/skorokhod/springcourse/TestSpring.java
+++ src/main/java/ru/skorokhod/springcourse/TestSpring.java
@@ -1,11 +1,11 @@
 package ru.skorokhod.springcourse;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
 
         ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
